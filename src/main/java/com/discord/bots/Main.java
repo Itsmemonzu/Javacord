a7bf6de84754346690b7dc009a7d2e575c063872
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 17:18:27 +0600

    initial bot

---
+++ src/main/java/com/discord/bots/Main.java
@@ -0,0 +1,21 @@
+package com.discord.bots;
+
+import com.discord.bots.commands.Ping;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static String Prefix = "!";
+
+    public static void main(String[] args){
+
+        DiscordApi Bot = new DiscordApiBuilder().setToken(System.getenv().get("TOKEN")).login().join();
+
+        Bot.addListener(new Ping());
+
+        System.out.println("Bot is online!");
+
+    }
+
+}
