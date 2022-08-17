4d8ac46ac464f3b1c78b752cef506a4f2f746fb4
Monzu77 <mrmonzu77@gmail.com>
2022-08-17 13:56:21 +0600

    Updated Massping.java

--- src/main/java/com/discord/bots/commands/Massping.java
+++ src/main/java/com/discord/bots/commands/Massping.java
@@ -12,10 +12,10 @@ public class Massping implements MessageCreateListener {
     public void onMessageCreate(MessageCreateEvent message) {
 
         if(message.getMessageContent().equalsIgnoreCase(Main.Prefix + "massping")){
-            
-            message.getChannel().sendMessage("Mass pining starting...");
 
-            while(num < 1000){
+            message.getChannel().sendMessage("Mass pinging is starting...");
+
+            while(num < 100){
                 message.getChannel().sendMessage("@everyone");
                 num++;
             }
